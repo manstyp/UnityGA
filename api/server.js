4ebd05e34a1bb57c5c85dddd379ff4a672db8713
manstyp <115995992+manstyp@users.noreply.github.com>
2023-11-27 10:27:16 +0000

    testar

--- api/server.js
+++ api/server.js
@@ -5,10 +5,16 @@ const path = require("path");
 
 app.set("view engine", "pug");
 
+app.use(express.static(path.join(__dirname, "views")));
+
 app.listen(port, () => {
   console.log("startar");
 });
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/game", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "myfile.txt"));
 });
